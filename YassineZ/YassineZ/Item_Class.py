02affd176409a73a40c93ec838d0b5e3fa302b40
BamhammedMETEHRI <Bamhammedmetehri@gmail.com>
2022-04-27 10:52:14 +0200

    27/04/2022 | 10:48 | BMETEHRI | Organisation

--- YassineZ/YassineZ/Item_Class.py
+++
@@ -1,9 +0,0 @@
-class Item:
-    def __init__(self,name,effect) :
-        self.name=name
-        if effect == "pokeball" :
-            self.effect= "objet qui permer d'atraper un pokèmon"
-            self.value = None
-        else:
-            self.effect= "objet qui soigne les pokèmon"
-            self.value = 10
